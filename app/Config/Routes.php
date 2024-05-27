1914bbe79644baef031b2f501d69f6c480290cd8
inraw <weyoune97@gmail.com>
2024-05-27 14:58:19 +0700

    tambah controller artikel,penulis,user dan routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,8 +5,22 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
 
 //Routes Aurthentication
 $routes->get('/login', 'Auth::index');
 $routes->post('/login/prosesLogin', 'Auth::prosesLogin');
+
+//routes dashboard
+$routes->get('/', 'Home::index');
+
+
+//routes artikel
+$routes->get('/artikel', 'Artikel::index');
+
+//routes penulis
+$routes->get('/penulis', 'Penulis::index');
+
+//routes user
+$routes->get('/user', 'User::index');
+
+
--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -5,11 +5,11 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 
-class Artikel extends BaseController
+class User extends BaseController
 {
     public function index()
     {
-        return view('artikel');
+        return view('user');
 
     }
 }
