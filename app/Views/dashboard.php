e525cf3da7b957063df0650604f99851ebd0e4be
RamziAhmar <144034481+RamziAhmar@users.noreply.github.com>
2024-06-13 11:31:41 +0700

    tambah dashboard

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -2,54 +2,50 @@
 <?= $this->section('content') ?>
 <!-- isi konten -->
 <div class="row">
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="card gradient-1">
-                            <div class="card-body">
-                                <h3 class="card-title text-white">Total Artikel</h3>
-                                <div class="d-inline-block">
-                                    <h2 class="text-white">4565</h2>
-                                    <p class="text-white mb-0">Jan - March 2019</p>
-                                </div>
-                                <span class="float-right display-5 opacity-5"><i class="fa fa-book"></i></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="card gradient-2">
-                            <div class="card-body">
-                                <h3 class="card-title text-white">Total Penulis</h3>
-                                <div class="d-inline-block">
-                                    <h2 class="text-white">$ 8541</h2>
-                                    <p class="text-white mb-0">Jan - March 2019</p>
-                                </div>
-                                <span class="float-right display-5 opacity-5"><i class="fa fa-pencil"></i></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="card gradient-3">
-                            <div class="card-body">
-                                <h3 class="card-title text-white">Total Pengunjung</h3>
-                                <div class="d-inline-block">
-                                    <h2 class="text-white"><?= esc($totalViews) ?></h2>
-                                    <p class="text-white mb-0">Jan - March 2019</p>
-                                </div>
-                                <span class="float-right display-5 opacity-5"><i class="fa fa-users"></i></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="card gradient-4">
-                            <div class="card-body">
-                                <h3 class="card-title text-white">Pengunjung Hari Ini</h3>
-                                <div class="d-inline-block">
-                                    <h2 class="text-white"><?= esc($totalViewsToday) ?></h2>
-                                    <p class="text-white mb-0">Jan - March 2019</p>
-                                </div>
-                                <span class="float-right display-5 opacity-5"><i class="fa fa-calendar"></i></span>
-                            </div>
-                        </div>
-                    </div>
+    <div class="col-lg-3 col-sm-6">
+        <div class="card gradient-1">
+            <div class="card-body">
+                <h3 class="card-title text-white">Total Artikel</h3>
+                <div class="d-inline-block">
+                    <h2 class="text-white"><?= $totalArtikel ?></h2>
                 </div>
+                <span class="float-right display-5 opacity-5"><i class="fa fa-book"></i></span>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-sm-6">
+        <div class="card gradient-2">
+            <div class="card-body">
+                <h3 class="card-title text-white">Total Penulis</h3>
+                <div class="d-inline-block">
+                    <h2 class="text-white"><?= $totalProfil ?></h2>
+                </div>
+                <span class="float-right display-5 opacity-5"><i class="fa fa-pencil"></i></span>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-sm-6">
+        <div class="card gradient-3">
+            <div class="card-body">
+                <h3 class="card-title text-white">Total Pengunjung</h3>
+                <div class="d-inline-block">
+                    <h2 class="text-white"><?= esc($totalViews) ?></h2>
+                </div>
+                <span class="float-right display-5 opacity-5"><i class="fa fa-users"></i></span>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-sm-6">
+        <div class="card gradient-4">
+            <div class="card-body">
+                <h3 class="card-title text-white">Pengunjung Hari Ini</h3>
+                <div class="d-inline-block">
+                    <h2 class="text-white"><?= esc($totalViewsToday) ?></h2>
+                </div>
+                <span class="float-right display-5 opacity-5"><i class="fa fa-calendar"></i></span>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?= $this->endSection() ?>
